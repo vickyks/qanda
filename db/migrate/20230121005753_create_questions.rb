e8c4107302d4d17c70478036ec0222dd0f828c79
Vicky Stephens <vicky.k.stephens@gmail.com>
2023-01-21 01:01:29 +0000

    Initial commit: Add models for Question, Answer and Devise user.

---
+++ db/migrate/20230121005753_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :questions do |t|
+      t.text :text
+      t.references :user, null: false, foreign_key: true
+      t.integer :answer_count
+
+      t.timestamps
+    end
+  end
+end
